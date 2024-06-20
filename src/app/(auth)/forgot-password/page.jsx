78cd34df981c37938ad172f53064552e6c1f6848
zubbybright <zubbybrightson@gmail.com>
2024-06-20 14:48:42 +0100

    navbar

---
+++ src/app/(auth)/forgot-password/page.jsx
@@ -0,0 +1,9 @@
+const ForgotPasswordPage = () => {
+    return (
+        <div>
+            Forgot Password Page
+        </div>
+    )
+}
+
+export default ForgotPasswordPage
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,36 @@
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+
+const Links = () => {
+
+    const links = [
+        {
+            title: "Homepage",
+            path: "/"
+        },
+        {
+            title: "About",
+            path: "/about"
+        },
+        {
+            title: "Contact",
+            path: "/contact"
+        },
+        {
+            title: "Blog",
+            path: "/blog"
+        },
+    ];
+
+    return (
+        <div className={styles.links}>
+            {
+                links.map((link => (
+                    <NavLink item={link} key={link.title}/>
+                )))
+            }
+        </div>
+    )
+}
+
+export default Links
